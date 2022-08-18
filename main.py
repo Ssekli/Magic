ebefa9519c712f9b67fcca303ccf2e3d2af927df
Ssekli <111066892+Ssekli@users.noreply.github.com>
2022-08-18 19:38:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 MIN_NUMBER = 1
 MAX_NUMBER = 10
 MAGIC_NUMBER = random.randint(MIN_NUMBER, MAX_NUMBER)
+NB_LIFES = 3
 
 def ask_number(MIN_NUMBER, MAX_NUMBER) :
     number_int = 0
@@ -21,12 +22,42 @@ def ask_number(MIN_NUMBER, MAX_NUMBER) :
 
 
 number = 0
+lifes = NB_LIFES
 
-while number != MAGIC_NUMBER:
+while number != MAGIC_NUMBER and lifes > 0:
+    print(f"Il vous reste {lifes} vies")
     number = ask_number(MIN_NUMBER, MAX_NUMBER)
     if number == MAGIC_NUMBER :
         print("Bravo, vous avez gagné ! ")
     elif number > MAGIC_NUMBER:
         print(f"Le nombre est plus petit que {number}")
+        lifes -= 1
     else:
         print(f"Le nombre est plus grand que {number}")
+        lifes -= 1
+
+if lifes == 0:
+    print (f"Vous avez perdu ! Le nombre était {MAGIC_NUMBER}")
+
+
+#Boucle for / Lifes
+
+"""
+gagne = False
+for i in range(0, NB_LIFES):
+    lifes = NB_LIFES-i
+    print(f"Il vous reste {lifes} vies")
+    number = ask_number(MIN_NUMBER, MAX_NUMBER)
+    if number == MAGIC_NUMBER :
+        print("Bravo, vous avez gagné ! ")
+        gagne = True
+        break
+    elif number > MAGIC_NUMBER:
+        print(f"Le nombre est plus petit que {number}")
+    else:
+        print(f"Le nombre est plus grand que {number}")
+    
+
+if not gagne:
+    print (f"Vous avez perdu ! Le nombre était {MAGIC_NUMBER}") 
+    """
