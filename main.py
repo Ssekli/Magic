deda3373e143e2382d0a5f757009c9590da52077
Ssekli <111066892+Ssekli@users.noreply.github.com>
2022-08-18 18:01:00 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def ask_number(MIN_NUMBER, MAX_NUMBER) :
             number_int = int(number_str)
         except ValueError:
             print("Erreur : Entrez un nombre")
-        
+        else:
             if number_int < MIN_NUMBER or number_int > MAX_NUMBER:
                 print(f"ERREUR : Entrez un nombre compris entre {MIN_NUMBER} et {MAX_NUMBER}")
                 number_int = 0
