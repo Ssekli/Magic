c0169f29dc0c1e05129aae124d4a5f60b1e7a322
Ssekli <111066892+Ssekli@users.noreply.github.com>
2022-08-23 10:32:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,32 @@
+# Creation de devinez nombre
 import random
 
 MIN_NUMBER = 1
 MAX_NUMBER = 10
-NB_QUESTIONS = 5
-NB_POINTS = 0 
+MAGIC_NUMBER = random.randint(MIN_NUMBER, MAX_NUMBER)
+
+def ask_number(MIN_NUMBER, MAX_NUMBER) :
+    number_int = 0
+    while number_int == 0:
+        number_str = input(f"Quel est le nombre magique (entre {MIN_NUMBER} et {MAX_NUMBER}) ? ")
+        try:
+            number_int = int(number_str)
+        except ValueError:
+            print("Erreur : Entrez un nombre")
+        else:
+            if number_int < MIN_NUMBER or number_int > MAX_NUMBER:
+                print(f"ERREUR : Entrez un nombre compris entre {MIN_NUMBER} et {MAX_NUMBER}")
+                number_int = 0
+    return number_int
 
 
-def ask_question():
-    a = random.randint(MIN_NUMBER, MAX_NUMBER)
-    b = random.randint(MIN_NUMBER, MAX_NUMBER)
-    o = random.randint(0, 2)
-    operateur_str = "+"
-    if o == 1:
-        operateur_str = "-"
-    elif o == 2:
-        operateur_str = "*"
+number = 0
 
-    answer_str = input(f"Quel est la réponse de {a} {operateur_str} {b} ?")
-    answer_int = int(answer_str)
-
-    calcul = a+b
-    if o ==1:
-        calcul = a-b
-    elif o ==2:
-        calcul = a*b
-
-    if answer_int == calcul:
-        return True
-    return False
-
-
-
-for i in range(0, NB_QUESTIONS):
-    print(f"Question {i+1}")
-    if ask_question():
-        print("Réponse correcte")
-        NB_POINTS += 1
+while number != MAGIC_NUMBER:
+    number = ask_number(MIN_NUMBER, MAX_NUMBER)
+    if number == MAGIC_NUMBER :
+        print("Bravo, vous avez gagné ! ")
+    elif number > MAGIC_NUMBER:
+        print(f"Le nombre est plus petit que {number}")
     else:
-        print("Réponse incorrecte")
-
-print(f"Votre note est {NB_POINTS}/{NB_QUESTIONS}")
-
-if NB_POINTS == int(NB_QUESTIONS):
-    print("Vous êtes excellent")
-elif NB_POINTS > float(NB_QUESTIONS) /2:
-    print("Pas mal")
-elif NB_POINTS == 0:
-    print("You suck")
+        print(f"Le nombre est plus grand que {number}")
